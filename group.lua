d77334632dbc1ad1b116e2142d45b2b210bb93eb
ejsvifq_mabmip <euloanty@live.com>
2019-06-20 18:51:53 +0800

    lfg m+

--- group.lua
+++ group.lua
@@ -1,34 +1,22 @@
 local LFG_OPT = LibStub("AceAddon-3.0"):GetAddon("LookingForGroup_Options")
 
 local function keystone_information(detailed)
-	for bagID = 0, 4 do
-		for invID = 1, GetContainerNumSlots(bagID) do
-			local itemID = GetContainerItemID(bagID, invID)
-			if itemID and itemID == 158923 then
-				local itemLink = GetContainerItemLink(bagID, invID)
-				local item_id, map, keyLevel, l4,l7 = string.match(itemLink, 'keystone:(%d+):(%d+):(%d+):(%d+):(%d+)')
-				if not detailed then
-					return keyLevel,tonumber(keyLevel)
-				end
-				local maps_to_activity_id =
-				{
-					[244] = 502,
-					[245] = 518,
-					[249] = 514,
-					[252] = 522,
-					[353] = 534,
-					[250] = 504,
-					[247] = 510,
-					[251] = 507,
-					[246] = 526,
-					[248] = 530
-				}
-				local activity_id = maps_to_activity_id[tonumber(map)]
-				if activity_id then
-					return activity_id,keyLevel,tonumber(keyLevel)
-				end
-			end
-		end
+	local maps_to_activity_id =
+	{
+		[244] = 502,
+		[245] = 518,
+		[249] = 514,
+		[252] = 522,
+		[353] = 534,
+		[250] = 504,
+		[247] = 510,
+		[251] = 507,
+		[246] = 526,
+		[248] = 530
+	}
+	local mapid = C_MythicPlus.GetOwnedKeystoneChallengeMapID()
+	if mapid then
+		return maps_to_activity_id[mapid],C_MythicPlus.GetOwnedKeystoneLevel()
 	end
 end
 
@@ -43,7 +31,7 @@ LFG_OPT:push("m+",{
 		{
 			order = 1,
 			name = function()
-				local t = keystone_information()
+				local t = C_MythicPlus.GetOwnedKeystoneLevel()
 				if t then
 					return t
 				end
@@ -69,13 +57,13 @@ LFG_OPT:push("m+",{
 					C_LFGList.RemoveListing()
 					return
 				end
-				local activityID,key_level,key_level_number = keystone_information(true)
+				local activityID,key_level_number = keystone_information(true)
 				if not activityID then
 					LFG_OPT.expected(disable_text)
 					return
 				end
-				if string.match(LFGListFrame.EntryCreation.Name:GetText(),"(%d+)") ~= key_level then
-					LFG_OPT.expected("|cffff0000Title does not contain the keyword|r "..key_level)
+				if string.match(LFGListFrame.EntryCreation.Name:GetText(),"(%d+)") ~= tostring(key_level_number) then
+					LFG_OPT.expected("|cffff0000Title does not contain the keyword|r "..key_level_number)
 					return
 				end
 				local _,_,categoryID,groupID = C_LFGList.GetActivityInfo(activityID)
@@ -115,6 +103,60 @@ LFG_OPT:push("m+",{
 			get = LFG_OPT.options_get_s_function,
 			set = LFG_OPT.options_set_s_function
 		},
+		desc = 
+		{
+			order = 5,
+			name = function()
+				local t = {}
+				local C_MythicPlus = C_MythicPlus
+				if C_MythicPlus.IsWeeklyRewardAvailable() then
+					t[#t+1] = "|cffff0000"
+					t[#t+1] = CLAIM_REWARD
+					t[#t+1] = "|r\n"
+				end
+				local best_kl = 10
+				local best_rw = C_MythicPlus.GetRewardLevelForDifficultyLevel(best_kl)
+				while best_kl < 31 do
+					local gg = C_MythicPlus.GetRewardLevelForDifficultyLevel(best_kl+3)
+					if gg == best_rw then
+						break
+					end
+					best_rw = gg
+					best_kl = best_kl + 5
+				end
+				local owned_keystone_level = C_MythicPlus.GetOwnedKeystoneLevel()
+				if owned_keystone_level then
+					t[#t+1] = format(MYTHIC_PLUS_MISSING_WEEKLY_CHEST_REWARD,owned_keystone_level,
+										C_MythicPlus.GetRewardLevelForDifficultyLevel(owned_keystone_level))
+				end
+				if owned_keystone_level ~= best_kl then
+					if owned_keystone_level then
+						t[#t+1] = "\n"
+					end
+					t[#t+1] = "|cffff0000"
+					t[#t+1] = format(MYTHIC_PLUS_MISSING_WEEKLY_CHEST_REWARD,best_kl,best_rw)
+					t[#t+1] = "|r"
+				end
+				local affixes = C_MythicPlus.GetCurrentAffixes()
+				if affixes then
+					t[#t+1] = "\n\n|cff8080cc"
+					t[#t+1] = #affixes
+					t[#t+1] = "|r"
+					for i=1,#affixes do
+						local name,description,filedataid = C_ChallengeMode.GetAffixInfo(affixes[i].id)
+						t[#t+1] = "\n|T"
+						t[#t+1] = filedataid
+						t[#t+1] = ":0:0:0:0:10:10:1:9:1:9|t|cff8080cc"
+						t[#t+1] = name
+						t[#t+1] = "|r\n"
+						t[#t+1] = description
+					end
+				end
+				return table.concat(t)
+			end,
+			fontSize = "large",
+			type = "description"
+		}
 	}
 })
 
